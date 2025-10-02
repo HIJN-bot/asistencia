7272790e66a759a5009a8bfe8af6fd9373005906
Samuel7510 <arkanargos@gmail.com>
2025-10-02 07:44:03 -0500

    Update header.php

--- QR-Code-Based-Attendance-System/QR-ATS-main/componets/header.php
+++ QR-Code-Based-Attendance-System/QR-ATS-main/componets/header.php
@@ -1,7 +1,10 @@
 <header class="header">
+    <!-- Título dinámico -->
     <h3 class="title"><?php echo $title; ?></h3>
+
+    <!-- Perfil del usuario -->
     <div class="profile" onclick="showBox()">
-        <img src="../resources/icons/2 User.svg" alt="" />
+        <img src="../resources/icons/2 User.svg" alt="Icono de usuario" />
         <span class="profile_name"><?php echo $username; ?></span>
     </div>
 </header>
